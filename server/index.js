0f06c49a22f46c535b0cae48351e30e3752d98f3
harrison-symes <symeshjb@gmail.com>
2017-11-02 16:53:33 +1300

    cmon

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var server = require('./server')
 
+var Knex = require('knex')
+var config = require('../knexfile')[process.env.NODE_ENV || "development"]
+var knex = knex(config)
+
 var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
   console.log('Listening on port', PORT)
 })
-
